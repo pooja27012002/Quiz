b808dc78301bdc9e5d197b4817979a6de1bf4561
POOJA JADHAV <92859828+pooja27012002@users.noreply.github.com>
2023-10-15 13:42:19 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+let questions = [
+  {
+      numb: 1,
+      question: "What does XML stand for?",
+      answer: "eXtensible Markup Language",
+      options: [
+          "eXtensible Markup Language",
+          "eXecutable Multiple Language",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+      ]
+  },
+  {
+      numb: 2,
+      question: "Which programming language is commonly used for adding interactivity to web pages?",
+      answer: "JavaScript",
+      options: [
+          "HTML",
+          "CSS",
+          "JavaScript",
+          "Python"
+      ]
+  },
+  {
+      numb: 3,
+      question: "What does CSS stand for in web development?",
+      answer: "Cascading Style Sheet",
+      options: [
+          "Cascading Style Sheet",
+          "Computer Style Sheet",
+          "Creative Style System",
+          "Colorful Style Selector"
+      ]
+  },
+  {
+      numb: 4,
+      question: "What does the typeof operator return when used with the value null in JavaScript?",
+      answer: "object",
+      
+      options: [
+        "undefined",
+        "object",
+        "null",
+        "number"
+    ]
+  },
+  {
+      numb: 5,
+      question: "What is the purpose of the JavaScript 'querySelector' method?",
+      answer: "To select the first element that matches a specified CSS selector",
+      options: [
+          "To select a random element on the web page",
+          "To select the first element that matches a specified CSS selector",
+          "To select all elements on the web page",
+          "To select elements by their tag name"
+      ]
+  },
+  {
+      numb: 6,
+      question: "Which of the following is NOT a valid HTML5 semantic element?",
+      answer: "div",
+      options: [
+          "header",
+          "div",
+          "nav",
+          "article"
+      ]
+  },
+  {
+      numb: 7,
+      question: "What is the role of a web server in web development?",
+      answer: "To respond to HTTP requests from clients",
+      options: [
+          "To design the user interface",
+          "To store and manage data",
+          "To respond to HTTP requests from clients",
+          "To run client-side scripts"
+      ]
+  },
+  {
+      numb: 8,
+      question: "Which of the following is a front-end web development framework/library?",
+      answer: "React",
+      options: [
+          "Angular",
+          "Node.js",
+          "Ruby on Rails",
+          "React"
+      ]
+  },
+  {
+      numb: 9,
+      question: "What is the purpose of the CSS property 'margin'?",
+      answer: "To add space around an element",
+      options: [
+          "To set the background color of an element",
+          "To add space inside an element",
+          "To add space around an element",
+          "To change the font size of text"
+      ]
+  },
+  {
+      numb: 10,
+      question: "What is the purpose of the HTML 'form' element in web development?",
+      answer: "To create an input area for user input",
+      options: [
+          "To define a clickable button",
+          "To create a container for text content",
+          "To create an input area for user input",
+          "To define a hyperlink"
+      ]
+  }
+];
